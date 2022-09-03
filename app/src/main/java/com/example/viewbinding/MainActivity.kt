7a002cd4175a1300e7f73e239c845969246500d9
ragioniere1980@gmail.com <ragioniere1980@gmail.com>
2022-09-03 19:08:36 +0300

    Initial commit

---
+++ app/src/main/java/com/example/viewbinding/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.viewbinding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
+import com.example.viewbinding.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+//        val messageView = findViewById<TextView>(R.id.tvMessage)
+//        val inputText = findViewById<EditText>(R.id.etName)
+//        val submitButton = findViewById<TextView>(R.id.btnSubmit)
+        binding.apply {
+            btnSubmit.setOnClickListener {
+                val name = etName.text.toString()
+                tvMessage.text = "Hello $name"
+                etName.setText("")
+            }
+        }
+    }
+}
